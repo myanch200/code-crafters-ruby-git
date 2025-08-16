a73638cfacc24d5061e03d2dd92977a6fdaa452b
Martin Yanchev <myanch200@gmail.com>
2025-08-16 12:08:20 +0100

    cat-file remove extra put statement

--- app/commands/cat_file.rb
+++ app/commands/cat_file.rb
@@ -12,9 +12,7 @@ module Commands
 
   def self.pretty_print(file_path)
     file_path = ".git/objects/#{file_path[0..1]}/#{file_path[2..-1]}"
-    puts file_path
     if File.exist?(file_path)
-      # remove the initial signiture of blob 11\0
       content = Zlib::Inflate.inflate(File.read(file_path)).sub(/^blob \d+\0/, "")
       puts content
     else
